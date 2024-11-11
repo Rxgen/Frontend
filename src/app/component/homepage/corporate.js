9be699aab58b4ef923364f5a150023d785da3097
Abhiman Sharma <abhiman@webmaffia.com>
2024-11-11 19:59:42 +0530

    Slider is added and css updated

--- src/app/component/homepage/corporate.js
+++ src/app/component/homepage/corporate.js
@@ -1,22 +1,65 @@
 import Image from "next/image" ;
+import Link from "next/link";
 
 
 
 export default function Corporate () {
     return (
-        <section data-section="corporate_banner" className="corporate_banner inner_banner" id="corporate_banner">
-        <picture>
-            <source media="(max-width: 540px)" srcSet="/images/homepage/banner/mobile/banner_bg_1.webp" />
-    <Image
-      src="/images/corporate/banner.webp"
-      alt=""
-      className="banner_img"
-      width="1920"
-      height="771"
-    />
-  </picture>
-  <h1 className="subtitle_66">Corporate Overview</h1>
+      <section data-section="corporate_overview" className="corporate_overview">
+      <div className="corporate_containers">
+          <h2 className="h2_title">CORPORATE OVERVIEW</h2>
+          <h3 className="subtitle_60">
+              Transforming hope
+              into healing.
+          </h3>
+          <p className="para scroll_content">
+              Lupin is a global pharmaceutical company whose
+              product development pipeline includes complex
+              inhalations branded generics projects, and a specialty
+              focus on women's health. As a global pharma company,
+              we work towards bringing novel products to the market
+              that address unmet medical needs.
+          </p>
+          <a href="corporate.php" className="black_cta">
+              <div className="cta_container">
+                  <span>Know more</span>
+                  <Image src="/images/icons/white_arrow.webp" alt="" width="20" height="14" />
+              </div>
+          </a>
+      </div>
+      <div className="corporate_numbers_container">
+          <div className="corporate_number_detail">
+              <div className="corporate_number">150+</div>
+              <div className="corporate_text">commercialized products in the US</div>
+          </div>
+          <div className="corporate_number_detail">
+              <div className="corporate_number">3rd Major</div>
+              <div className="corporate_text">pharma company in the US, by number of prescriptions filled</div>
+          </div>
+          <div className="corporate_number_detail">
+              <div className="corporate_number">5M</div>
+              <div className="corporate_text">number of patient we reached in the US</div>
+          </div>
+          <div className="corporate_number_detail">
+              <div className="corporate_number">15</div>
+              <div className="corporate_text">manufacturing facilities globally</div>
+          </div>
+          <div className="corporate_number_detail">
+              <div className="corporate_number">7 R&D</div>
+              <div className="corporate_text">centers globally</div>
+          </div>
+          <div className="corporate_number_detail">
+              <div className="corporate_number">100+</div>
+              <div className="corporate_text">countries reached</div>
+          </div>
+      </div>
+      <div className="flower_bg_container">
+          <div className="flower_bg">
+              <Image src="/images/flower_bg.webp" alt="" width="895" height="851" />
+          </div>
+      </div>
   </section>
+  
     );
   };
   
--- src/app/component/layout/header.js
+++ src/app/component/layout/header.js
@@ -5,6 +5,7 @@ import Image from "next/image";
 
 const Header = ({}) => {
     return (
+      <div >
       <header>
 	<a href="" className="lupin_logo">
 		<Image src="/images/lupin_logo.webp" alt="" width="134" height="49" />
@@ -34,7 +35,17 @@ const Header = ({}) => {
 	<div className="menu_icons">
 		<a href="" className="language_icon"><Image src="/images/icons/language.webp" alt="" width="32" height="32" /></a>
 	</div>
+  
 </header>
+<a href="" className="menu">
+	<div className="menu_icon">
+		<span className="span_menu"></span>
+		<span className="span_menu bottom_span_menu"></span>
+	</div>
+	<span className="menu_text">Menu</span>
+</a>
+</div>
+
     );
   };
   
