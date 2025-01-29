c534b30a623e4a8eb2c8c040e70e320bb091fa76
Abhiman Sharma <abhiman@webmaffia.com>
2025-01-29 12:14:54 +0530

    Leadership Banner changed

--- src/app/component/leadership/topbanner.js
+++ src/app/component/leadership/topbanner.js
@@ -6,8 +6,8 @@ export default function Topbanner() {
     return (
       <section data-section="contact_banner" className="product_listing_banner inner_banner banner_section">
       <picture>
-          <source media="(max-width: 540px)" srcSet="/images/leadership/Leadership_mob_banner.png" />
-          <Image src="/images/leadership/banner.webp" alt="Leadesrhip Banner" className="banner_img" width="1920" height="767" />
+          <source media="(max-width: 540px)" srcSet="/images/leadership/leadrship_mob.webp" />
+          <Image src="/images/leadership/leadership_banner.webp" alt="Leadesrhip Banner" className="banner_img" width="1920" height="767" />
       </picture>
       <h1 className="subtitle_66">Leadership</h1>
   </section>
