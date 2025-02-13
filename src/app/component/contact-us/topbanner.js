d3ff0159bce8d4c481e5d864c729a59cd7997f47
Abhiman Sharma <abhiman@webmaffia.com>
2025-02-13 11:36:09 +0530

    Contact Us Banner updated

--- src/app/component/contact-us/topbanner.js
+++ src/app/component/contact-us/topbanner.js
@@ -1,36 +1,217 @@
-"use client"
+"use client";
 import Image from "next/image";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import SvgMap from "./svgmap";
 
-export default function TopBanner({ topbannerdata }) {
-    console.log("Banner Data DIsplay",topbannerdata);
-    const getMediaUrl = (url) => `${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}${url}`;
-  
-    const desktopImageUrl = topbannerdata?.desktop_image?.url
-      ? getMediaUrl(topbannerdata.desktop_image.formats.large.url)
-      : "assets/images/about/banner.webp";
-  
-    const mobileImageUrl = topbannerdata?.mobile_image?.url
-      ? getMediaUrl(topbannerdata.mobile_image.formats.small.url)
-      : "assets/images/about/mob_banner.webp";
-  
-      
-    const bannerText = topbannerdata?.banner_text || "Cotact Us";
-  
-    return (
-        
-<section data-section="contact_banner" className="product_listing_banner inner_banner banner_section">
-        <picture>
-          <source media="(max-width: 540px)" srcSet={mobileImageUrl} />
-          <Image
-            src={desktopImageUrl}
-            alt={bannerText}
-            className="banner_img"
-            width="1920"
-            height="771"
-          />
-        </picture>
-        <h1 className="subtitle_66">{bannerText}</h1>
-      </section>
-     
+export default function TopBanner() {
+  const countries = [
+    {
+      countryCode: "CA",
+      countryClass: "canada",
+      companyLogoSrc: "/images/contact/icons/canada.webp",
+      companyLogoWidth: 91,
+      companyLogoHeight: 33,
+      flagSrc: "/images/globe/img_11.webp",
+      pinSrc: "/images/contact/pin_img.webp",
+    },
+    {
+      countryCode: "US",
+      countryClass: "pharma",
+      companyLogoSrc: "/images/contact/icons/pharma.webp",
+      companyLogoWidth: 95,
+      companyLogoHeight: 35,
+      flagSrc: "/images/globe/img_12.webp",
+      pinSrc: "/images/contact/pin_img.webp",
+    },
+    {
+      countryCode: "MX",
+      countryClass: "grin",
+      companyLogoSrc: "/images/contact/icons/grin.webp",
+      companyLogoWidth: 184,
+      companyLogoHeight: 65,
+      flagSrc: "/images/globe/img_9.webp",
+      pinSrc: "/images/contact/pin_img.webp",
+    },
+    {
+      countryCode: "BR",
+      countryClass: "med",
+      companyLogoSrc: "/images/contact/icons/med.webp",
+      companyLogoWidth: 162,
+      companyLogoHeight: 28,
+      flagSrc: "/images/globe/img_10.webp",
+      pinSrc: "/images/contact/pin_img.webp",
+    },
+    {
+      countryCode: "UK",
+      countryClass: "healthCare",
+      companyLogoSrc: "/images/contact/icons/health.webp",
+      companyLogoWidth: 81,
+      companyLogoHeight: 35,
+      flagSrc: "/images/globe/img_3.webp",
+      pinSrc: "/images/contact/pin_img.webp",
+    },
+    {
+      countryCode: "NL",
+      countryClass: "nanomi",
+      companyLogoSrc: "/images/contact/icons/nanomi.webp",
+      companyLogoWidth: 77,
+      companyLogoHeight: 22,
+      flagSrc: "/images/globe/img_8.webp",
+      pinSrc: "/images/contact/pin_img.webp",
+    },
+    {
+      countryCode: "GE",
+      countryClass: "hormosan",
+      companyLogoSrc: "/images/contact/icons/hormoson.webp",
+      companyLogoWidth: 115,
+      companyLogoHeight: 12,
+      flagSrc: "/images/globe/img_7.webp",
+      pinSrc: "/images/contact/pin_img.webp",
+    },
+    {
+      countryCode: "CH",
+      countryClass: "lahsal",
+      companyLogoSrc: "/images/contact/icons/lahsal.webp",
+      companyLogoWidth: 100,
+      companyLogoHeight: 100,
+      flagSrc: "/images/globe/img_2.webp",
+      pinSrc: "/images/contact/pin_img.webp",
+    },
+    {
+      countryCode: "SA",
+      countryClass: "dynamic",
+      companyLogoSrc: "/images/contact/icons/dynamic.webp",
+      companyLogoWidth: 179,
+      companyLogoHeight: 38,
+      flagSrc: "/images/globe/img_4.webp",
+      pinSrc: "/images/contact/pin_img.webp",
+    },
+    {
+      countryCode: "IN",
+      countryClass: "lupinIn",
+      companyLogoSrc: "/images/contact/icons/lupin.webp",
+      companyLogoWidth: 45,
+      companyLogoHeight: 54,
+      flagSrc: "/images/globe/img_1.webp",
+      pinSrc: "/images/contact/pin_img.webp",
+    },
+    {
+      countryCode: "PH",
+      countryClass: "multiCare",
+      companyLogoSrc: "/images/contact/icons/multicare.webp",
+      companyLogoWidth: 151,
+      companyLogoHeight: 26,
+      flagSrc: "/images/globe/img_6.webp",
+      pinSrc: "/images/contact/pin_img.webp",
+    },
+    {
+      countryCode: "AU",
+      countryClass: "genericHealth",
+      companyLogoSrc: "/images/contact/icons/generic.webp",
+      companyLogoWidth: 144,
+      companyLogoHeight: 43,
+      flagSrc: "/images/globe/img_5.webp",
+      pinSrc: "/images/contact/pin_img.webp",
+    },
+  ];
+  const [activeCountryCode, setActiveCountryCode] = useState(null);
+  let interval;
+
+  const toggleActiveClass = () => {
+    const currentCountryIndex = countries.findIndex(
+      (country) => country.countryCode === activeCountryCode
     );
-  }
+    const nextCountryIndex = (currentCountryIndex + 1) % countries.length;
+    const nextCountryCode = countries[nextCountryIndex].countryCode;
+
+    const previousPath = document.querySelector(`#${activeCountryCode}`);
+    if (previousPath) {
+      previousPath.classList.remove('active');
+    }
+
+    const countryPath = document.querySelector(`#${nextCountryCode}`);
+    if (countryPath) {
+      countryPath.classList.add('active');
+    }
+    setActiveCountryCode(nextCountryCode);
+  };
+
+  
+  useEffect(() => {
+    interval = setInterval(toggleActiveClass, 2000); 
+    return () => clearInterval(interval); 
+  }, [activeCountryCode]); 
+
+  const handleCountryHover = (countryCode) => {
+    setActiveCountryCode(countryCode);
+    const previousPath = document.querySelector(`#${activeCountryCode}`);
+    if (previousPath) {
+      previousPath.classList.remove('active');
+    }
+    const countryPath = document.querySelector(`#${countryCode}`);
+    if (countryPath) {
+      countryPath.classList.add('active');
+    }
+  };
+
+
+  const handleMouseLeave = () => {
+    if (activeCountryCode) {
+      const countryPath = document.querySelector(`#${activeCountryCode}`);
+      if (countryPath) {
+        countryPath.classList.remove('active');
+      }
+      setActiveCountryCode(null);  
+    }
+  };
+     
+  return (
+    
+      <section
+        data-section="contact_banner_section" className="contact_banner inner_banner banner_section"
+      >
+        <div className="map_container">
+        <SvgMap />
+    {countries.map((country, index) => (
+            <a
+              key={country.countryCode}
+              href="#"
+              data-map=""
+              data-country={country.countryCode}
+              className={`country ${country.countryClass} ${index === countries.findIndex(c => c.countryCode === activeCountryCode) ? 'active' : ''}`}
+              onMouseEnter={() => handleCountryHover(country.countryCode)} 
+              onMouseLeave={handleMouseLeave} 
+            >
+              <div className="country_company">
+                <Image
+                  src={country.companyLogoSrc}
+                  alt={country.countryCode}
+                  width={country.companyLogoWidth}
+                  height={country.companyLogoHeight}
+                />
+              </div>
+              <div className="country_logo">
+                <Image
+                  src={country.flagSrc}
+                  alt={`${country.countryCode} flag`}
+                  width={38}
+                  height={26}
+                  className="flag_logo"
+                />
+                <Image
+                  src={country.pinSrc}
+                  alt={`${country.countryCode} pin`}
+                  width={681}
+                  height={1213}
+                  className="pin_logo"
+                />
+              </div>
+            </a>
+          ))}
+        </div>
+        <h1 className="subtitle_66">Contact Us</h1>
+      </section>
+    
+  );
+}
