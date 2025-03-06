04d931901f54631c2610cae0f380f25fc938da6d
Abhiman Sharma <abhiman@webmaffia.com>
2025-03-06 11:51:32 +0530

    page media changes

--- src/app/media/page.js
+++ src/app/media/page.js
@@ -2,8 +2,9 @@ import { fetchNewstData } from "../component/homepage/Api/fetchnews";
 import NewsBanner from "../component/media/newsbanner";
 import News from "../component/media/news";
 
-export default async function Newsdata() {
-  const { pressnews} = await fetchNewstData();
+export default async function Newsdata({searchParams}) {
+  const { search = "", page = 1 } = searchParams;
+  const { pressnews} = await fetchNewstData(page, 9, search);
   console.log("Server component data", pressnews)
     return (
         <div>
