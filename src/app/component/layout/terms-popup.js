e56c2a12c46eb4c33826ba4f8019da1836c88793
Abhiman Sharma <abhiman@webmaffia.com>
2025-03-07 19:59:04 +0530

    CHanges are done

--- src/app/component/layout/terms-popup.js
+++ src/app/component/layout/terms-popup.js
@@ -15,7 +15,7 @@ export default function TermsPopup() {
     <div id="terms_popup" className={`guide_popup disclaimer_popup terms_popup ${isActive ? "active" : ""}`}>
   <div className="popup_content">
     <div className="guide_container">
-      <div className="subtitle_30">We've updated our terms</div>
+      <div className="subtitle_30">We&apos;ve updated our terms</div>
       <div className="disclaimer_para">
         We encourage you to review our updated <Link href="/images/dummy.pdf" target="_blank">Terms of Use</Link>, <Link href="/privacy-policy" target="_blank">Privacy Statement</Link>, and Privacy Policy. <br />By continuing, you agree to the
         updated Terms listed here.
