4c33dbd7d1584f61353fbecb32d69eab03841170
Abhiman Sharma <abhiman@webmaffia.com>
2025-03-27 15:21:42 +0530

    Video issue for mobile

--- src/app/component/homepageClient.js
+++ src/app/component/homepageClient.js
@@ -5,13 +5,16 @@ import News from "./homepage/news";
 import Offering from "./homepage/offering";
 import Sustainable from "./homepage/sustainable";
 import People from "./homepage/people";
+import { headers } from "next/headers";
 
 export default function HomePageClient({ bannersData, corporateData, aboutData ,newsData, peopleData, offeringData,sustainabilityData}) {
   
+  const userAgent = headers().get("user-agent") || "";
+  const isMobile = /Mobi|Android/i.test(userAgent);
   return (
     <div>
       
-      <Homebanner banners={bannersData} />
+      <Homebanner banners={bannersData} isServerMobile={isMobile}/>
       <Corporate corporateData={corporateData} />
       <About aboutData={aboutData} />
       <News  newsData={newsData}/>
