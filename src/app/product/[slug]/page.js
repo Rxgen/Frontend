f0160364ebb42edd941a0afa67b02165eb23d4fe
Abhiman Sharma <abhiman@webmaffia.com>
2025-03-31 14:14:37 +0530

    Product redirection is added in slug page file

---
+++ src/app/product/[slug]/page.js
@@ -0,0 +1,65 @@
+import { fetchProductBySlug } from "@/app/component/homepage/Api/fetchProduct";
+import ProductDetails  from "@/app/component/product/productdetails";
+import DetailBanner from "@/app/component/product/productdetailbanner";
+import Filter from "@/app/component/product/filter";
+import { redirect } from "next/navigation";
+
+const slugMappings = {
+  "amlodipine-besylate-tablets-usp": "amlodipine-besylate-and-benazepril-hcl-capsules-usp",
+  "gavilyte-n":"gavi-lyte-n",
+};
+
+
+
+ export async function generateMetadata({ params }) {
+  const { slug } = params;
+
+
+  try {
+    const productSeoDetails = await fetchProductBySlug(slug);
+    console.log("Meta title and escription details",productSeoDetails)
+
+    if (!productSeoDetails || productSeoDetails.length === 0) {
+      return {
+        title: productSeoDetails[0].product_name,
+        description: " ",
+      };
+    }
+
+    const { seo } = productSeoDetails[0] || {};
+    const { metaTitle, metaDescription } = seo || {};
+
+    return {
+      title: metaTitle || productSeoDetails[0].product_name,
+      description: metaDescription || " ",
+      canonical: `${process.env.NEXT_PUBLIC_BASE_URL}/products/${slug}`,
+    };
+  } catch (error) {
+    console.error("Error fetching product details:", error);
+    return {
+      title: "Error",
+      description: "An error occurred while fetching product details.",
+    };
+  }
+} 
+
+export default async function Page({ params }) {
+  const { slug } = params;
+
+  if (slugMappings[slug]) {
+    console.log(`Redirecting from /product/${slug} to /product/${slugMappings[slug]}`);
+    redirect(`/product/${slugMappings[slug]}`);
+  }
+
+  console.log(`Fetching product details for slug: ${slug}`);
+ 
+  const productdetails = await fetchProductBySlug(slug);
+
+  return (
+    <div>
+      <DetailBanner />
+      <Filter /> 
+      <ProductDetails productdata={productdetails } />  
+    </div>
+  )
+}
---
+++ src/app/product/page.js
@@ -0,0 +1,32 @@
+import { fetchProducts } from "../component/homepage/Api/fetchProduct";
+import Products from "../component/product/productlist";
+import Filter from "../component/product/filter";
+import Productbanner from "../component/product/productbanner";
+
+export const generateMetadata = () => {
+  return {
+    title: 'Explore Our Products | Quality Medicines for Better Health.',
+    description: "Browse our range of high-quality medicines focused on better patient outcomes",
+  };
+};
+
+export default async function ProductsPage({ searchParams }) {
+  const { letter = "", page = "1" , category = "" , new_products="",brand = "" , ndc= "", productName= ""} = searchParams; 
+  console.log("Category api call " , category);
+  const { products, totalPages,totalproduct} = await fetchProducts(letter || "", page, category || "" , new_products || "" ,brand || "", productName || "" , ndc || "");
+
+  console.log("Server" , products );
+ 
+  
+
+  return (
+    <div> 
+      <Productbanner />
+       <Filter selectedLetter={letter || ""} selectedCategory={category || ""}   selectedBrand={brand} />
+        <Products productdata={products} totalPages={totalPages} currentPage={parseInt(page, 10)} totalproduct={totalproduct}/>
+      
+    </div>
+  );
+}
+
+
