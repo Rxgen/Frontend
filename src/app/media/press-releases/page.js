b75afcc09699cdf4fe404dbbec37d1bfd443ad36
Ravi Chauhan <ravi.chauhan@webmaffia.com>
2025-07-25 12:32:53 +0530

    Press realese meta title and description made dynamic

--- src/app/media/press-releases/page.js
+++ src/app/media/press-releases/page.js
@@ -2,17 +2,42 @@ import { fetchNewstData } from "@/app/component/homepage/Api/fetchnews";
 import PressNewsBanner from "@/app/component/press-releases/press-banner";
 import PressNews from "@/app/component/press-releases/press-news";
 
-export const generateMetadata = ({ params}) => {
-  const path="press-releases"
-  
+
+export async function generateMetadata({ params }) {
+  const { slug } = params;
+
+
+  try {
+   const NewsSeoData = await fetchNewsdetails(slug);
+    console.log("Meta title and escription details",NewsSeoData)
+
+    if (!NewsSeoData || NewsSeoData.length === 0) {
+      return {
+        title: NewsSeoData[0].news_title,
+        description: "",
+      };
+    }
+
+    const { seo } = NewsSeoData[0] || {};
+    const { metaTitle, metaDescription } = seo || {};
+
     return {
-      title: 'About  Press-Releases',
-      description: 'Lupin US is dedicated to enhancing global health with innovative pharmaceutical solutions. Discover our history, values, purpose, and more.',
+      title: metaTitle || NewsSeoData[0].news_title,
+      description: metaDescription || "Test3",
       alternates: {
-        canonical: `${process.env.NEXT_PUBLIC_BASE_URL}${path}`,
+        canonical:`${process.env.NEXT_PUBLIC_BASE_URL}media/${slug}`,
       }
     };
-  };
+  } catch (error) {
+    console.error("Error fetching product details:", error);
+    return {
+      title: "Error",
+      description: "An error occurred while fetching product details.",
+    };
+  }
+} 
+
+
 
 
 export default async function Newsdata({searchParams}) {
