7738155c172529f4a3d2661c8f373577439ef7ec
Ravi Chauhan <ravi.chauhan@webmaffia.com>
2025-09-15 18:29:30 +0530

    Media  schema is added

--- src/app/media/[slug]/page.js
+++ src/app/media/[slug]/page.js
@@ -1,6 +1,7 @@
 import { fetchNewsdetails } from "@/app/component/homepage/Api/fetchnews";
 import Newsdetailbanner from "@/app/component/media/newsdetailbanner";
 import Newsdetail from "@/app/component/media/newsdetail";
+import Script from "next/script";
 
 
 export async function generateMetadata({ params }) {
@@ -37,12 +38,89 @@ export async function generateMetadata({ params }) {
   }
 } 
 
+
+     // Media Schema 
+const MediaSchema = (news, slug) => {
+  const { seo } = news || {};
+  const { metaDescription } = seo || {};
+
+  return {
+    "@context": "https://schema.org",
+    "@type": "NewsArticle",
+    "mainEntityOfPage": {
+      "@type": "WebPage",
+      "@id": `${process.env.NEXT_PUBLIC_BASE_URL}media/${slug}`,
+    },
+    "headline": news?.news_title || "N/A",
+    "description": metaDescription || "",  
+    "author": {
+      "@type": "Organization",
+      "name": "Lupin US",
+      "url": "https://www.lupin.com/US",
+    },
+    "publisher": {
+      "@type": "Organization",
+      "name": "Lupin US",
+      "logo": {
+        "@type": "ImageObject",
+        "url": `${process.env.NEXT_PUBLIC_BASE_URL}/images/lupin_logo.webp`,
+      },
+    },
+    "datePublished": news?.publishedAt || "",
+  };
+};
+
+
 export default async function Newsdetaildata({params}) {
 
   const { slug } = params;
       const Newsdetaildata = await fetchNewsdetails(slug);
+      const news = Newsdetaildata?.[0];
+
+// Breadcrumb Schema
+  const breadcrumbSchema = {
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    itemListElement: [
+      {
+        "@type": "ListItem",
+        position: 1,
+        name: "Home",
+        item: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+      },
+      {
+        "@type": "ListItem",
+        position: 2,
+        name: "Media",
+        item: `${process.env.NEXT_PUBLIC_BASE_URL}media`,
+      },
+      {
+        "@type": "ListItem",
+        position: 3,
+        name: news?.news_title || "N/A",
+        item: `${process.env.NEXT_PUBLIC_BASE_URL}media/${slug}`,
+      },
+    ],
+  };
+
+
     return (
         <div>
+
+          <Script
+        id="breadcrumb-schema"
+        type="application/ld+json"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumbSchema) }}
+      /> 
+
+      {/* Media Schema */}
+      <Script
+        id="Media-schema"
+        type="application/ld+json"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(MediaSchema(news, slug)) }}
+      />   
           <Newsdetailbanner />
           <Newsdetail Newsdetaildata={Newsdetaildata} />
         </div>
