ad633d0662ce3c7ff6734885f50b425dc11ef055
Ravi Chauhan <ravi.chauhan@webmaffia.com>
2025-09-15 14:11:16 +0530

    Breadcrumb schema and product schema is implemented

--- src/app/products/[slug]/page.js
+++ src/app/products/[slug]/page.js
@@ -2,6 +2,7 @@ import { fetchProductBySlug } from "@/app/component/homepage/Api/fetchProduct";
 import ProductDetails  from "@/app/component/product/productdetails";
 import DetailBanner from "@/app/component/product/productdetailbanner";
 import Filter from "@/app/component/product/filter";
+import Script from "next/script";
 
 
 
@@ -36,12 +37,106 @@ import Filter from "@/app/component/product/filter";
   }
 } 
 
+ const getMediaUrl = (url) => `${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}${url}`;
+
+const ProductSchema = (product) => ({
+  "@context": "https://schema.org/",
+  "@type": "Product",
+  "name": product?.product_name || "N/A",
+  "image": getMediaUrl(product.product_images[0].slide.url) || "",
+  "description": product?.description || "",
+  "brand": {
+    "@type": "Brand",
+    "name": product?.brand || "N/A",
+  },
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "4.7",
+    "reviewCount": "185",
+  },
+  "additionalProperty": [
+    {
+      "@type": "PropertyValue",
+      "name": "Strength",
+      "value": product.strength || "N/A",
+    },
+    {
+      "@type": "PropertyValue",
+      "name": "Pack Size",
+      "value": product.pack_size || "N/A"
+    },
+    {
+      "@type": "PropertyValue",
+      "name": "NDC#",
+      "value": product.ndc || "N/A"
+    },
+    {
+      "@type": "PropertyValue",
+      "name": "RLD/Brand Name",
+      "value": product.brand.name || "N/A"
+    },
+    {
+      "@type": "PropertyValue",
+      "name": "TE Rating",
+      "value": product.te_rating || "N/A"
+    },
+    {
+      "@type": "PropertyValue",
+      "name": "Therapeutic Category",
+      "value": product.category.name || "N/A"
+    }
+  ],
+  "manufacturer": {
+    "@type": "Organization",
+    "name": "Lupin US",
+    "logo": `${process.env.NEXT_PUBLIC_BASE_URL}images/lupin_logo.webp`,
+    "url": `${process.env.NEXT_PUBLIC_BASE_URL}`,
+  }
+});
+
+
+
+
 export default async function fetchproductdetails({ params }){
   const { slug } = params;
   const productdetails = await fetchProductBySlug(slug);
+  const product = productdetails?.[0];
+
+  const breadcrumbSchema = {
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    itemListElement: [
+      {
+        "@type": "ListItem",
+        position: 1,
+        name: "Home",
+        item: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+      },
+      {
+        "@type": "ListItem",
+        position: 2,
+        name: product?.product_name || "N/A",
+        item: `${process.env.NEXT_PUBLIC_BASE_URL}products/${slug}`,
+      },
+    ],
+  };
 
   return (
     <div>
+      <Script
+        id="breadcrumb-schema"
+        type="application/ld+json"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumbSchema) }}
+      /> 
+
+      {/* Product Schema */}
+      <Script
+        id="organization-schema"
+        type="application/ld+json"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(ProductSchema(product)) }}
+      />   
       <DetailBanner />
       <Filter /> 
       <ProductDetails productdata={productdetails } />  
